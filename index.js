e42e0547ac7476c1ff510f1d35ac3df3b83c58ca
Trevor Antonio <codertrev@proton.me>
2026-06-04 15:27:47 -0700

    Instantiated new files, started building out clock

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+/*const decrementBtn = document.getElementById("decrementBtn")
+const resetBtn = document.getElementById("resetBtn")
+const incrementBtn = document.getElementById("incrementBtn")
+let count = 0
+
+function increase() {
+
+    count++
+    let increase = count++
+    console.log(increase)
+} */
